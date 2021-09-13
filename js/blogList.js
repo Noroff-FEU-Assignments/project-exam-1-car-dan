0ee0daac05d69dcd2bd7e9e07849ba5c351117bb
car-dan <73527175+car-dan@users.noreply.github.com>
2021-09-13 12:56:43 +0200

    v21

--- js/blogList.js
+++ js/blogList.js
@@ -1,6 +1,9 @@
 const baseUrl = "https://carolinedanielrud.one/wp-json/wp/v2/posts?_embed";
 const extraUrl = "https://carolinedanielrud.one/wp-json/wp/v2/posts?_embed&per_page=100";
 const blogList = document.querySelector(".blogList");
+const button = document.querySelector(".btn");
+let itemsDisplayed = 10;
+
 
 
 async function getPosts(url) {
@@ -8,23 +11,43 @@ async function getPosts(url) {
         const response = await fetch(url);
         const posts = await response.json();
         console.log(posts);
+        console.log(posts.length)
+
 
 
         blogList.innerHTML = " ";
 
-        posts.forEach(function (post) {
+        for (let i = 0; i < itemsDisplayed; i++) {
             blogList.innerHTML += `
-             <a href="blog.html?id=${post.id}" class="postItem">
-                <div class="postImage">
-                        <img src="${post._embedded['wp:featuredmedia']['0'].source_url}" class="featuredMedia" alt=""/>
-                </div>
-                <div class="postTitle">
-                        <h2>${post.title.rendered}</h2>
-                </div>
-            </a>
+                <a href="blog.html?id=${posts[i].id}" class="postItem">
+                         <div class="postImage">
+                             <img src="${posts[i]._embedded['wp:featuredmedia']['0'].source_url}"     class="featuredMedia" alt=""/>
+                         </div>
+                         <div class="postTitle">
+                             <h2>${posts[i].title.rendered}</h2>
+                         </div>
+                    </a>
             `
-            console.log(post.id);
-        });
+        };
+
+        function displayMore(event) {
+            console.log("clicked");
+            for (let i = itemsDisplayed; i < posts.length && (itemsDisplayed + 10); i++) {
+                blogList.innerHTML += `
+                <a href="blog.html?id=${posts[i].id}" class="postItem">
+                         <div class="postImage">
+                             <img src="${posts[i]._embedded['wp:featuredmedia']['0'].source_url}"     class="featuredMedia" alt=""/>
+                         </div>
+                         <div class="postTitle">
+                             <h2>${posts[i].title.rendered}</h2>
+                         </div>
+                    </a>
+            `
+            };
+            itemsDisplayed += 10;
+        };
+
+
 
 
     }
@@ -33,10 +56,21 @@ async function getPosts(url) {
             <div class="error">
                 <p>Something went wrong, please try agian later</p>
             </div>
-        `;
-    }
+        `
+    };
 
-}
+
+
+
+    button.addEventListener("click", displayMore);
+
+};
 
 getPosts(extraUrl);
+
+
+
+
+
+
 
