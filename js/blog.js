aaedc792a04c78ff15bea822925c26eb48e19608
car-dan <73527175+car-dan@users.noreply.github.com>
2021-09-18 13:45:24 +0200

    hotjar

--- js/blog.js
+++ js/blog.js
@@ -5,47 +5,37 @@ const url = "https://carolinedanielrud.one/wp-json/wp/v2/posts";
 const blogPost = document.querySelector(".blogPost");
 const modal = document.querySelector(".modal");
 
-console.log(id);
 
 async function getPost() {
     try {
         const response = await fetch(url);
         const posts = await response.json();
 
-        console.log(posts[8].content);
-
         posts.forEach(function (post) {
             if (post.id != id) {
-
-            }
-            else {
-
+            } else {
                 blogPost.innerHTML = `
                         <h1>${post.title.rendered}</h1>
                         <div class = "gallery">${post.content.rendered}</div>
                         <div class = "text">${post.excerpt.rendered}</div>
                         </div>
                 `
-
-
             }
-        })
-        // console.log(post[0].content.rendered);
+        });
+
         const gallery = document.querySelector(".gallery");
-        console.log(gallery)
+
 
         function handleInteraction(event) {
-            // event.preventDefault();
+            event.preventDefault();
             if (event.target.classList.contains("modal")) {
                 modal.classList.remove("open");
             } else if (event.target = gallery) {
-                console.log(event.currentTarget)
                 modal.classList.add("open");
                 modal.innerHTML = `
-            <img src="${event.target.src}" alt="" class="full-img" />
-        `;
-            }
-        }
+            <img src="${event.target.src}" alt="" class="full-img" /> `;
+            };
+        };
 
         gallery.addEventListener(`click`, handleInteraction);
         modal.addEventListener(`click`, handleInteraction);
@@ -61,37 +51,6 @@ async function getPost() {
 
 }
 
-// const img = document.querySelectorAll(".gallery.wp-block-image");
-// console.log(img + "this")
-
-
-// blogPost.addEventListener(`touchstart`, handleInteraction);
-
-// modal.addEventListener(`touchstart`, handleInteraction);
-
-
-// window.addEventListener(`click`, handleInteraction);
-
-// window.addEventListener("click", function (event) {
-//     if (event.target.classList.contains("modal")) {
-//         modal.classList.remove("open");
-//     } else {
-//         console.log(event.target.src)
-//         modal.classList.add("open");
-//         modal.innerHTML = `
-//             <img src="${event.target.src}" alt="" class="full-img" />
-//         `;
-//     }
-
-
-// });
-
-// modal.addEventListener("click", (e) => {
-//     if (e.target.classList.contains("modal")) {
-//         // modal.classList.remove("open");
-//         modal.style.opacity = "0";
-//     }
-// });
 
 
 
